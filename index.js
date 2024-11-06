8d95a27097ea7090c4ddf76db3ec94d9aad925d8
Winner Chinedu <chineduwinnerchibundo@gmail.com>
2024-11-06 14:15:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+require('dotenv').config() 
+const PORT =  process.env.PORT ||4700 
+const mongoose = require('mongoose')
+const mongodb_uri = process.env.URI
+const cors = require('cors')
+const bodyParser = require('body-parser')
+app.use(cors ())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+const NigeriaFoods =[
+    {
+        id:1234, 
+        location: 'ibadan',
+        price: 600, 
+    },
+    {
+        id:2, 
+        location:'Oyo', 
+        price: 120,
+    },
+    {
+        id: 3, 
+        location: 'Osun',
+        price: 3400
+    }, 
+    {
+        id: 5, 
+        location: 'Sokoto', 
+        price: 2500
+    }
+]
+
+mongoose.connect(mongodb_uri)
+.then(() =>{
+    console.log('Database connected');  
+})
+.catch((err) =>{
+    console.log(err);
+})
+
+app.get('/home', (req, res) =>{
+    res.send(NigeriaFoods)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Lift off! Server has started at ${PORT}`);  
+})
